2574cd794856fcb9dcc8ac9b0bfa9e4de5241de7
Wilson Sam <wilson.sam@tykhegaming.com>
2017-11-21 15:31:13 +0530

    Win Number Wrong Documentation Resolved

--- src/main/scala/roulette/BillboardApp.scala
+++ src/main/scala/roulette/BillboardApp.scala
@@ -11,38 +11,41 @@ import device.io.usb.Pl2303
 import display.io.WindowConfig
 import monix.execution.Scheduler
 import monix.execution.schedulers.SchedulerService
-import monix.reactive.Observable
+//import monix.reactive.Observable
 import pureconfig._
 import roulette.State.Running
 import roulette.ecs.{BillboardScene, BillboardSceneRB}
-import scodec.bits.ByteVector
-
-import scala.concurrent.duration._
+//import scodec.bits.ByteVector
+//
+//import scala.concurrent.duration._
 
 object BillboardApp extends App {
 
   implicit val scheduler: SchedulerService = Scheduler.fixedPool("usb", 4)
-  implicit val usbConfig: UsbConfig = UsbConfig()
+  implicit val usbConfig: UsbConfig = UsbConfig(uart = Uart(stopBits = 1) )
 
   //main thread will wait until gate is open - gate is open when ui thread on complete.
   val latch = new CountDownLatch(1)
 
+  //Read application.conf
   val conf = ConfigFactory.load
-
-  val file = File(conf.getString("persistence.file"))
-
-  val seed: State = if (file.exists) {
-    file.readDeserialized[Running]
-  } else {
-    file.createIfNotExists(asDirectory = false, createParents = true)
-    file.writeSerialized(Running("EU-01", Seq.empty[String], 100, 100, 10000))
-    file.readDeserialized[Running]
-  }
-
+  val databaseFile = File(conf.getString("persistence.file"))
   val config = loadConfigOrThrow[WindowConfig]("window")
 
-  val window = if(conf.getString("app.theme") == "red-black") BillboardSceneRB(seed) else BillboardScene(seed)
+  //Load seed based on databaseFile
+  val seed: Running = if (databaseFile.exists) {
+    databaseFile.readDeserialized[Running]
+  } else {
+    databaseFile.createIfNotExists(asDirectory = false, createParents = true)
+    databaseFile.writeSerialized(Running("EU01", Seq.empty[String], 100, 1000, 100000))
+    databaseFile.readDeserialized[Running]
+  }
 
+  //Select the window based on app theme
+  val window = conf.getString("app.theme") match {
+    case "red-black" => BillboardSceneRB(seed)
+    case _ => BillboardScene(seed)
+  }
 
   device.io.usb.Hub() match {
     case util.Success(hub) =>
@@ -50,32 +53,37 @@ object BillboardApp extends App {
         case util.Success(dp) =>
           val (scene, ui) = dp.unicast
 
-          val device = Observable.interval(10.seconds)
-            .map { x => (math.random() * 37).toInt }
-            .debug("number")
-            .map { s => (" " + s).takeRight(2) }
-            .map(s => ByteVector(s.toCharArray.map(_.toByte)).bits)
-            .debug("bits")
+//          val device = Observable.interval(10.seconds)
+//            .map { _ => (math.random() * 37).toInt }
+//            .debug("number")
+//            .map { s => (" " + s).takeRight(2) }
+//            .map(s => ByteVector(s.toCharArray.map(_.toByte)).bits)
+//            .debug("bits")
 
           hub.scan(Pl2303).foreach {
             case DeviceAttached(port) =>
+              println(s"$port attached")
               hub.open(port)
                 .drive(Input.codec)
                 .liftByOperator(debug[Input]("protocol"))
                 .collect {
                   case Win(num) => Event.SpinCompleted(num)
+//                  case Status(BallDetected,x,num,y,z,a) => Event.SpinCompleted(num)
                 }.unicast._2.foreach(scene.onNext)
             case DeviceDetached(port) =>
               println(s"$port detached")
           }
-          device.decode(Input.codec)
-            .debug("protocol")
-            .collect {
-              case Win(num) => Event.SpinCompleted(num)
-            }.foreach(scene.onNext)
+
+//          device.decode(Input.codec)
+//            .debug("protocol")
+//            .collect {
+//              case Win(num) => Event.SpinCompleted(num)
+//            }.foreach(scene.onNext)
+
 
           // latch gate is open when ui thread on complete.
-          ui.doOnTerminate(_ => latch.countDown()).foreach(s => file.writeSerialized(s))
+          ui.doOnTerminate(_ => latch.countDown()).foreach(s => databaseFile.writeSerialized(s))
+
         case util.Failure(ex) =>
           ex.printStackTrace()
           latch.countDown()
